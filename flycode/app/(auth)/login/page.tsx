20dee80bfc60a8edd78e3f37b7bc7e600d78cf3a
JAGADEESHWARAN20 <jagadeeshwaransp5@gmail.com>
2025-04-15 15:17:46 +0530

    first commit

--- flycode/app/(auth)/login/page.tsx
+++ flycode/app/(auth)/login/page.tsx
@@ -26,7 +26,7 @@ export default function LoginPage() {
                const { error: authError } = await supabase.auth.signInWithOAuth({
                     provider: 'google',
                     options: {
-                         redirectTo: 'http://flycode.vercel.app/auth/callback'
+                         redirectTo: '/dashboard'
                     }
                });
 
