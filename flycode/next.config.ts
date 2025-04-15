f333781fa171d16cc35bd3e3c4b3512592de3024
JAGADEESHWARAN20 <jagadeeshwaransp5@gmail.com>
2025-04-15 12:35:28 +0530

    first commit

--- flycode/next.config.ts
+++ flycode/next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  rewrites: async () => [
+    {
+      source: '/',
+      destination: '/app',
+    },
+  ],
 };
 
 export default nextConfig;
